a67380107cdaf60781d217674bc55b66e7809642
kevin-rignault <kevin.rignault@gmail.com>
2015-09-21 23:17:47 +0200

    Push to Github

---
+++ Swift-Share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Share
+//
+//  Created by Kévin Rignault on 21/09/2015.
+//  Copyright © 2015 Kévin Rignault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
